73cde523f169455b812c225e8450a646b61c9b2f
MOHAMMAD Aima <mohammad.aima2402@gmail.com>
2026-04-20 23:10:39 +0200

    Update model version and adjust temperature in API call

--- llm_call.py
+++ llm_call.py
@@ -5,16 +5,21 @@ import os
 load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-response = client.chat.completions.create(
-    model="llama-3.1-8b-instant", #ID of the model to use
-    messages=[ # A list of messages comprising the conversation so far.
-        {"role":"system", "content":"Tu es un assistant utile."},
-        {"role":"user", "content" : "Explique- moi ce q'est un agent IA en 3 lignes."}
-    ],
- 
+try:
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile", #ID of the model to use
 
-    temperature= 0.1, #What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
-    max_tokens=200 #The maximum number of tokens that can be generated in the chat completion. The total length of input tokens and generated tokens is limited by the model's context length.
-)
+        messages=[ # A list of messages comprising the conversation so far.
+            {"role":"system", "content":"Tu es un assistant utile."},
+            {"role":"user", "content" : "Explique- moi ce q'est un agent IA en 3 lignes."}
+        ],
+    
 
-print(response.choices[0].message.content)
+        temperature= 0.7, #What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
+        max_tokens=200 #The maximum number of tokens that can be generated in the chat completion. The total length of input tokens and generated tokens is limited by the model's context length.
+
+    )
+    print(response.choices[0].message.content)
+
+except Exception as e:
+    print(f"Erreur API : {e}")
